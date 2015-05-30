b36f2691a7f2c29569d865fde0da72be318ce27f
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-05-30 10:02:09 -0400

    v1.2.2

--- del.js
+++ del.js
@@ -1,10 +1,11 @@
-//Video.js DEL (Doing Ending Leaving) v1.2
+//Video.js DEL (Doing Ending Leaving) v1.2.2
 
 function del(videoname, fullscreen) {
   var player = videojs(videoname, { }, function() {
     if (fullscreen) {
       this.requestFullScreen();
     }
+    document.getElementById(videoname).style.display = "visible";
     this.play(); 
     this.on('ended', function() {
       this.cancelFullScreen();
